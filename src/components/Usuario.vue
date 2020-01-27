577a7fb3f989eb89b67a228bd7cf0bdc54acbdf1
Pablo Souza <pvrsouza@gmail.com>
2020-01-27 07:21:08 -0300

    props e

--- src/components/Usuario.vue
+++ src/components/Usuario.vue
@@ -1,40 +1,52 @@
 <template>
-    <div class="container">
-        <h1>Componente Usuário</h1>
-        <p>Esse é um componente muito legal!</p>
-        <hr>
-        <div class="componentes">
-            <app-usuario-info />
-            <app-usuario-editar />
-        </div>
+  <div class="container">
+    <h1>Componente Usuário</h1>
+    <p>Esse é um componente muito legal!</p>
+    <p>O nome é: <strong>{{ nome }}</strong></p>
+    <button @click="alterarNome">Alterar nome</button>
+    <hr />
+    <div class="componentes">
+      <app-usuario-info :nome="nome" />
+      <app-usuario-editar />
     </div>
-</template>
+  </div>
+</template> 
 
 <script>
-import AppUsuarioInfo from './UsuarioInfo'
-import AppUsuarioEditar from './UsuarioEditar'
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar }
-}
+  components: { AppUsuarioInfo, AppUsuarioEditar },
+  data() {
+    return {
+      nome: "Pablo"
+    };
+  },
+  methods: {
+    alterarNome() {
+      this.nome = "Ana";
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .componentes {
-        display: flex;
-    }
+.componentes {
+  display: flex;
+}
 
-    .componentes > * {
-        margin: 10px;
-    }
+.componentes > * {
+  margin: 10px;
+}
 </style>
--- src/components/UsuarioInfo.vue
+++ src/components/UsuarioInfo.vue
@@ -1,20 +1,39 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do usuário:
+      <strong>{{ inverterNome() }}</strong>
+    </p>
+    <button @click="reiniciaNome">Reiniciar Nome</button>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  props: {
+    nome: {
+      type: String,
+      //required: true
+      default: "Anônimo" //caso
+    }
+  },
+  methods: {
+    inverterNome() {
+      return this.nome
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
